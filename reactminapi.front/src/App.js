b1d3608e3ce0f5ddfdecedeb72446c94bcecb713
Elio Cortes <elio.cortes3000@gmail.com>
2022-10-14 16:54:04 -0500

    Implement pagination and documentation

--- reactminapi.front/src/App.js
+++ reactminapi.front/src/App.js
@@ -4,95 +4,100 @@ const App = () => {
     const pageSize = 10;
     const [users, setUsers] = useState([]);
     const [pages, setPages] = useState(0);
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [trainings, setTrainings] = useState([]);
-    const [initialIndex, setInitialIndex] = useState(0);
-    const [finalIndex, setFinalIndex] = useState(0);
-    const [selected, setSelected] = useState('');
+    const [recordsCount, setRecordsCount] = useState(0);
     const [loading, setLoading] = useState(true);
+   
     const populateUsersData = async () => {
         const response = await fetch('Users');
         const data = await response.json();
         setUsers(data);
-        setLoading(false);
     };
 
     const populateTrainingsData = async (userId) => {
         const response = await fetch(`AllTrainingsByUser?userId=${userId}`);
         const data = await response.json();
-        setPages(Math.round(data.length / pageSize));
-        setInitialIndex(currentPage * pageSize);
-        setFinalIndex(((currentPage + 1) * pageSize) - 1);
-        if (currentPage === 0) { setCurrentPage(currentPage + 1); }
-        setTrainings(data.slice(initialIndex, finalIndex));
+        setTrainings(data);
+        setRecordsCount(data.length);
+        setPages(Math.ceil(data.length / pageSize));
     };
+
     const handleChange = (e) => {
-        setSelected(e.target.value);
-        setLoading(true);
-    }
-    const clickPrevious = () => {
-        setCurrentPage(currentPage - 1);
-        setLoading(true);
-    }
-    const clickNext = () => {
-        setCurrentPage(currentPage + 1);
-        setLoading(true);
-    }
-    useEffect(() => {
-        if (loading) {
-            //if (users.length === 0) { 
-                populateUsersData();
-            //}
-            populateTrainingsData(selected);
+        let userId = e.target.value;
+        setCurrentPage(1);
+         if (userId !== '') {
+            populateTrainingsData(userId);
         }
-    }, [loading]);
+        else {
+            setTrainings([]);
+            setRecordsCount(0);
+            setPages(0);
+        }
+    }
+    const changePage = (page) => {
+        setCurrentPage(page);
+    }
+
+    useEffect(() => {
+        populateUsersData();
+        setLoading(false);
+     }, []);
+ 
+    const x = (currentPage * pageSize) - pageSize;
+    const y = x + pageSize;
+    const currentTrainings = trainings.slice(x, y);
     return (
         <div className="container">
             <div className="header-container">
-                {loading === true && trainings.length === 0
-                    ? <p><em>Loading... Please refresh once the API backend has started.</em></p>
+                {loading === true
+                       ? <p><em>Loading... Please refresh once the API backend has started.</em></p>
                     : <select onChange={handleChange}>
                         <option value="">Select user</option>
                         {users.map(user =>
-                            <option value={user.id}>{user.name}</option>
+                            <option key={user.id} value={user.id}>{user.name}</option>
                         )}
                     </select>
                 }
             </div>
             <div className="indexes">
-                <label className="pager">Initial: {initialIndex} </label>
-                <label className="pager">Final: {finalIndex}</label>
+                <label className="pager">Records: {recordsCount}</label>
+                <label className="pager">Pages: {pages}</label>
             </div>
             <div className="body-container">
-                {trainings && trainings.length > 0 && selected !== ''
+                {currentTrainings && currentTrainings.length > 0 
                     ? <table>
                         <thead>
-                            <th>distance</th>
-                            <th>duration</th>
-                            <th>date</th>
-                            <th>start hour</th>
-                            <th>location</th>
-                            <th>feel</th>
+                            <tr>
+                                <th>distance</th>
+                                <th>duration</th>
+                                <th>date</th>
+                                <th>start hour</th>
+                                <th>location</th>
+                                <th>feel</th>
+                            </tr>
                         </thead>
                         <tbody>
-                            {trainings.map(training =>
-                                <tr key={training.id}>
-                                    <td>{training.distance}</td>
-                                    <td>{training.duration}</td>
-                                    <td>{training.date}</td>
-                                    <td>{training.starthour}</td>
-                                    <td>{training.location}</td>
-                                    <td>{training.feel}</td>
-                                </tr>
-                            )}
+                            {currentTrainings.map(training => {
+                                return (
+                                    <tr key={training.id}>
+                                        <td>{training.distance}</td>
+                                        <td>{training.duration}</td>
+                                        <td>{training.date}</td>
+                                        <td>{training.starthour}</td>
+                                        <td>{training.location}</td>
+                                        <td>{training.feel}</td>
+                                    </tr>
+                                );
+                            })}
                         </tbody>
                     </table>
                     : <p>Data not found</p>}
             </div>
             <div className="footer-container">
-                <button disabled={currentPage <= 1 ? true : false} onClick={clickPrevious}>Previous page</button>
+                <button disabled={currentPage <= 1 ? true : false} onClick={() => changePage(currentPage - 1)}>Previous page</button>
                 <label className="pager">Current: {currentPage} of {pages}</label>
-                <button disabled={currentPage === pages ? true : false} onClick={clickNext}>Next page</button>
+                <button disabled={currentPage >= pages ? true : false} onClick={() => changePage(currentPage + 1)}>Next page</button>
             </div>
         </div>
     )
