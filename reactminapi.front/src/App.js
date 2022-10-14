5f036f451ba14136652add0687a84d06f14658fc
Elio Cortes <elio.cortes3000@gmail.com>
2022-10-13 22:15:00 -0500

    Added initial paging

--- reactminapi.front/src/App.js
+++ reactminapi.front/src/App.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
+    const pageSize = 10;
     const [users, setUsers] = useState([]);
+    const [pages, setPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
     const [trainings, setTrainings] = useState([]);
+    const [initialIndex, setInitialIndex] = useState(0);
+    const [finalIndex, setFinalIndex] = useState(0);
     const [selected, setSelected] = useState('');
     const [loading, setLoading] = useState(true);
     const populateUsersData = async () => {
@@ -15,22 +20,36 @@ const App = () => {
     const populateTrainingsData = async (userId) => {
         const response = await fetch(`AllTrainingsByUser?userId=${userId}`);
         const data = await response.json();
-        setTrainings(data);
+        setPages(Math.round(data.length / pageSize));
+        setInitialIndex(currentPage * pageSize);
+        setFinalIndex(((currentPage + 1) * pageSize) - 1);
+        if (currentPage === 0) { setCurrentPage(currentPage + 1); }
+        setTrainings(data.slice(initialIndex, finalIndex));
     };
     const handleChange = (e) => {
         setSelected(e.target.value);
         setLoading(true);
-     }
+    }
+    const clickPrevious = () => {
+        setCurrentPage(currentPage - 1);
+        setLoading(true);
+    }
+    const clickNext = () => {
+        setCurrentPage(currentPage + 1);
+        setLoading(true);
+    }
     useEffect(() => {
-        if (loading) { 
-            populateUsersData();
-            selected!== '' && populateTrainingsData(selected);
+        if (loading) {
+            //if (users.length === 0) { 
+                populateUsersData();
+            //}
+            populateTrainingsData(selected);
         }
     }, [loading]);
     return (
-        <div>
-            <div className="headerContainer">
-                {loading === true
+        <div className="container">
+            <div className="header-container">
+                {loading === true && trainings.length === 0
                     ? <p><em>Loading... Please refresh once the API backend has started.</em></p>
                     : <select onChange={handleChange}>
                         <option value="">Select user</option>
@@ -40,30 +59,41 @@ const App = () => {
                     </select>
                 }
             </div>
-            {trainings && trainings.length > 0
-                ? <table>
-                    <thead>
-                        <th>distance</th>
-                        <th>duration</th>
-                        <th>date</th>
-                        <th>start hour</th>
-                        <th>location</th>
-                        <th>feel</th>
-                    </thead>
-                    <tbody>
-                        {trainings.map(training =>
-                            <tr key={training.id}>
-                                <td>{training.distance}</td>
-                                <td>{training.duration}</td>
-                                <td>{training.date}</td>
-                                <td>{training.starthour}</td>
-                                <td>{training.location}</td>
-                                <td>{training.feel}</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
-                : <p>Data not found</p>}
+            <div className="indexes">
+                <label className="pager">Initial: {initialIndex} </label>
+                <label className="pager">Final: {finalIndex}</label>
+            </div>
+            <div className="body-container">
+                {trainings && trainings.length > 0 && selected !== ''
+                    ? <table>
+                        <thead>
+                            <th>distance</th>
+                            <th>duration</th>
+                            <th>date</th>
+                            <th>start hour</th>
+                            <th>location</th>
+                            <th>feel</th>
+                        </thead>
+                        <tbody>
+                            {trainings.map(training =>
+                                <tr key={training.id}>
+                                    <td>{training.distance}</td>
+                                    <td>{training.duration}</td>
+                                    <td>{training.date}</td>
+                                    <td>{training.starthour}</td>
+                                    <td>{training.location}</td>
+                                    <td>{training.feel}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    : <p>Data not found</p>}
+            </div>
+            <div className="footer-container">
+                <button disabled={currentPage <= 1 ? true : false} onClick={clickPrevious}>Previous page</button>
+                <label className="pager">Current: {currentPage} of {pages}</label>
+                <button disabled={currentPage === pages ? true : false} onClick={clickNext}>Next page</button>
+            </div>
         </div>
     )
 }
